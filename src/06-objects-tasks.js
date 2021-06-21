4ee1f0744866d75a8e5d32296bf77f8ed119c495
reipone <81408961+reipone@users.noreply.github.com>
2021-06-21 20:53:20 +0300

    feat: object task complete

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,8 +20,10 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => this.width * this.height;
 }
 
 
@@ -35,8 +37,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +53,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  Object.setPrototypeOf(obj, proto);
+  return obj;
 }
 
 
@@ -109,34 +113,103 @@ function fromJSON(/* proto, json */) {
  *
  *  For more examples see unit tests.
  */
-
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  order: ['type', 'id', 'class', 'attr', 'pclass', 'pelement'],
+  currentOrder: 0,
+
+  element(value) {
+    if (this.bType) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    if (this.currentOrder > this.order.indexOf('type')) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    const builder = Object.assign(Object.create(cssSelectorBuilder), this);
+    builder.bType = value;
+    builder.currentOrder = this.order.indexOf('type');
+    return builder;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    if (this.bId) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    if (this.currentOrder > this.order.indexOf('id')) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    const builder = Object.assign(Object.create(cssSelectorBuilder), this);
+    builder.bId = `#${value}`;
+    builder.currentOrder = this.order.indexOf('id');
+    return builder;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    if (this.currentOrder > this.order.indexOf('class')) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    const builder = Object.assign(Object.create(cssSelectorBuilder), this);
+    if (builder.bClass) {
+      builder.bClass += `.${value}`;
+    } else {
+      builder.bClass = `.${value}`;
+    }
+    builder.currentOrder = this.order.indexOf('class');
+    return builder;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    if (this.currentOrder > this.order.indexOf('attr')) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    const builder = Object.assign(Object.create(cssSelectorBuilder), this);
+    if (builder.bAttr) {
+      builder.bAttr += `[${value}]`;
+    } else {
+      builder.bAttr = `[${value}]`;
+    }
+    builder.currentOrder = this.order.indexOf('attr');
+    return builder;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    if (this.currentOrder > this.order.indexOf('pclass')) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    const builder = Object.assign(Object.create(cssSelectorBuilder), this);
+    if (builder.bPseudoClass) {
+      builder.bPseudoClass += `:${value}`;
+    } else {
+      builder.bPseudoClass = `:${value}`;
+    }
+    builder.currentOrder = this.order.indexOf('pclass');
+    return builder;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    if (this.bPseudoElem) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    const builder = Object.assign(Object.create(cssSelectorBuilder), this);
+    builder.bPseudoElem = `::${value}`;
+    builder.currentOrder = this.order.indexOf('pelement');
+    return builder;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    this.bCombination = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return this;
+  },
+
+  stringify() {
+    let result = '';
+    if (this.bCombination) {
+      result = this.bCombination;
+      this.clear();
+      return result;
+    }
+    if (this.bType) result += this.bType;
+    if (this.bId) result += this.bId;
+    if (this.bClass) result += this.bClass;
+    if (this.bAttr) result += this.bAttr;
+    if (this.bPseudoClass) result += this.bPseudoClass;
+    if (this.bPseudoElem) result += this.bPseudoElem;
+    this.clear();
+    return result;
+  },
+
+  clear() {
+    this.bType = '';
+    this.bId = '';
+    this.bClass = '';
+    this.bAttr = '';
+    this.bPseudoClass = '';
+    this.bPseudoElem = '';
+    this.bCombination = '';
+    this.currentOrder = 0;
   },
 };
 
